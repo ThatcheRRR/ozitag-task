a355e4a7ef2f1c1548f6fe441a28f60c31746246
ThatcheRRR <pavelosipov1377@gmail.com>
2020-10-19 23:41:20 +0300

    feat: add post request

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const App = () => {
+  useEffect(() => {
+    fetch('https://tager.dev.ozitag.com/api/auth/user', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({
+        "clientId": 1,
+        "email": "user@ozitag.com",
+        "password": "user"
+      })
+    }).then(data => { console.log(data); return data.json() }).then(data => console.log(data))
+  }, []);
+
   return(
     <h1>Test</h1>
   )
